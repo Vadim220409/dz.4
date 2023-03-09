001f8f0d7106b13ac3d8483d35e8e0df8ec96b6c
Vadim220409 <107919314+Vadim220409@users.noreply.github.com>
2023-03-09 19:38:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let number1 = 3;
+while(nimber <= 30) {
+    console.log(number);
+    number1++;
+}
+
+let number2 = 5;
+while(number >= -10) {
+    console.log(number2);
+    number2--;
+}
+
+let correct = 0
+let pwd;
+while(correct != pwd) {
+    pwd = prompt("Enter number");
+}
